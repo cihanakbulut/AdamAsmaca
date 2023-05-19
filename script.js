e41e96600ebea2c7658bb9ffd6cb910cfd4cdb6c
cihanakbulut <113177795+cihanakbulut@users.noreply.github.com>
2023-05-19 18:43:35 +0300

    CSS and Basic JS Codes

---
+++ script.js
@@ -0,0 +1,34 @@
+const word_el = document.getElementById('word');
+
+const correctLetters = ['j','a','v','s','t','r'];
+const wrongLetter = [];
+
+
+function getRandomWord(){
+    const words = ["javascript","java","python"];
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+
+
+function displayWord() {
+    const selectedWord = getRandomWord();
+
+    word_el.innerHTML =`
+    ${selectedWord.split('').map(letter => `
+        <div class="letter">
+            ${correctLetters.includes(letter) ? letter: ``}
+            </div>
+
+        
+            `).join('')}
+    
+    
+            `;
+
+           
+            const w = word_el.innerText.replace(/\n/g,'')
+            if (w ===selectedWord)
+            console.log('Bildiniz!')
+} 
+displayWord()
